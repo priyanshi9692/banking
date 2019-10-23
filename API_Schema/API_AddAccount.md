645bb4f781c9c33b64d3d9d594b960393206ed04
Priyanshi Jajoo <priyanshi.jajoo@sjsu.edu>
2019-10-22 22:01:23 -0700

    Sign Up form backend API to add new customer

--- API_Schema/API_AddAccount.md
+++ API_Schema/API_AddAccount.md
@@ -126,7 +126,22 @@
 + AA-50000:Unknown Error.     	
     	
     	
-    	
+    
+API: /sign-up, Type: POST
+	JSON Example: 		
+	{
+  firstname: 'Priyanshi',
+  lastname: 'Jajoo',
+  bdate: '2019-10-02',
+  ssn: '873459',
+  email: 'priyanshi.jajoo@sjsu.edu',
+  password: 'ouoafsaf',
+  address: 'Apt 80, 1800 Stokes Street',
+  city: 'San Jose',
+  state: 'Califonia',
+  zipcode: '95126',
+  remember: 'on'
+}
     	
     	
     	
--- routes/index.js
+++ routes/index.js
@@ -70,4 +70,35 @@ router.get('/customer-info', function(req,res,next){
   });
 });
 
+router.post('/sign-up', function(req,res, next) {
+  console.log("Input from the user:",req.body);
+  var newCustomer = {};
+  newCustomer.firstname = req.body.firstname;
+  newCustomer.lastname = req.body.lastname;
+  newCustomer.bdate = req.body.bdate;
+  newCustomer.ssn = req.body.ssn;
+  newCustomer.email = req.body.email;
+  newCustomer.password = req.body.password;
+  newCustomer.address = req.body.address;
+  newCustomer.city = req.body.city;
+  newCustomer.state = req.body.state;
+  newCustomer.zipcode = req.body.zipcode;
+
+  var con = mysql.createConnection(database);
+  con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected to DB!");
+  var sql = "INSERT INTO customer SET ?";
+  con.query(sql,newCustomer,function(err,result){
+    if (err) throw err;
+    else {
+      console.log("New Customer Data inserted successfully");
+    }
+    res.send("Successfully signed up!!");
+  });
+});
+});
+
+
+
 module.exports = router;
