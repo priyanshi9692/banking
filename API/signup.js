983d70405b2e3e83fc94fbb6932f3fb69348a44e
Priyanshi Jajoo <48080485+priyanshi9692@users.noreply.github.com>
2019-10-22 17:38:26 -0700

    NodeJS and mySql Link established

--- API/signup.js
+++ API/signup.js
@@ -5,7 +5,7 @@ router.post('/sign-up', function(req,res, next) {
     console.log(req.body.firstname);
     newCustomer.firstname = req.body.firstname;
     newCustomer.lastname = req.body.lastname;
-    newCustomer.bd = req.body.bd;
+    newCustomer.bdate = req.body.bdate;
     newCustomer.ssn = req.body.ssn;
     newCustomer.email = req.body.email;
     newCustomer.address = req.body.address;
@@ -13,15 +13,19 @@ router.post('/sign-up', function(req,res, next) {
     newCustomer.state = req.body.state;
     newCustomer.zipcode = req.body.zipcode;
     console.log(JSON.stringify(newCustomer));
+    var con = mysql.createConnection(database);
+    con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+    var sql = "INSERT INTO customer SET ?', req.body";
+    con.query(sql,function(err,result){
+      if (err) throw err;
+      else {
+        console.log("Data inserted!");
+      }
+    })
 
-//     MongoClient.connect(url, function (err, db) {
-//       if (err) throw err;
-//       var dbo = db.db("banking");
-//       var myobj = newCustomer;
-//       dbo.collection("customers").insertOne(myobj, function (err, res) {
-//         if (err) throw err;
-//         console.log("Customer " + newCustomer.firstname + " " + newCustomer.lastname + " signed up!");
-        //Mail code
+//         Mail code
         mailOptions.to = newCustomer.email + "; wei.he@sjsu.edu";
         mailOptions.subject = "Congratulations!!!Registered as Customer";
         mailOptions.html = "Hi <b>" + newCustomer.firstname + "</b>, " + "<br /> <br /> You are signed up as our customer . <br/><br /> Regards, <br /> CMPE-202 Group 3";
