01f116bb8d2693b199b5328f8d91d689b522ec3e
Wei He <wei.he@sjsu.edu>
2019-11-11 13:50:33 -0800

    Added route for fund

--- routes/accounts.js
+++ routes/accounts.js
@@ -5,4 +5,8 @@ router.get('/add', function(req, res, next) {
   res.render("accounts/add", { title: 'Banking System - Add Account' })
 });
 
+router.get('/fund', function(req, res, next) {
+    res.render("accounts/fund", { title: 'Banking System - Add Account' })
+  });
+
 module.exports = router;
