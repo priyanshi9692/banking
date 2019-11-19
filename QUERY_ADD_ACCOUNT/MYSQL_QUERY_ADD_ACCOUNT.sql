eaf89504a0e892ec60d300b8773091e7c11ae40a
Wei He <wei.he@sjsu.edu>
2019-11-19 12:06:00 -0800

    Updated sql for adding account

--- QUERY_ADD_ACCOUNT/MYSQL_QUERY_ADD_ACCOUNT.sql
+++ QUERY_ADD_ACCOUNT/MYSQL_QUERY_ADD_ACCOUNT.sql
@@ -1,12 +1,15 @@
 CREATE TABLE account (
-  	acc_num int NOT NULL AUTO_INCREMENT,
+  	acc_num int(10) ZEROFILL NOT NULL AUTO_INCREMENT,
   	customer_id int NOT NULL,
-  	branch_id varchar(255) NOT NULL,
-  	routing_num int NOT NULL,
+  	routing_num varchar(255) NOT NULL,
   	acc_type varchar(45) NOT NULL,
   	balance_amt float NOT NULL,
   	currency varchar(45) NOT NULL,
-  	open_date date NOT NULL,
+  	open_date datetime NOT NULL,
   	PRIMARY KEY (acc_num),
-	FOREIGN KEY (customer_id) REFERENCES customer(id)
+	FOREIGN KEY fk_customer_acct(customer_id)
+	REFERENCES customer(id)
 );
+
+INSERT INTO account (customer_id, routing_num, acc_type, balance_amt, currency, open_date)
+VALUES (4, '00001', 'Checking', 200.00, "$",  current_timestamp());
