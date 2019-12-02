24838786b3f287ffcecc0af88795841ce704d38c
Wei He <wei.he@sjsu.edu>
2019-12-01 21:42:54 -0800

    Use Promise to simplify async calls.

--- routes/accounts.js
+++ routes/accounts.js
@@ -20,10 +20,6 @@ var transporter = nodemailer.createTransport({
     }
   });
 
-// let customerInfo = {}
-// let customer_id = ""
-// let routing_num = ""
-// let customer_email = ""
 router.get('/addacct', function(req, res, next) {
     // ************* need to remove **********
     req.session.email = 'breehope@me.com'
@@ -82,16 +78,16 @@ router.post('/addacct', function(req, res, next) {
     newAcct.currency = "$";// hard code
     
     var con = mysql.createConnection(database);
+    
     con.connect(function(err) {
         if (err) throw err;
-        console.log("Connected!");
+
         // Get customer ID by email
         var sql = "SELECT id "
                 + "FROM customer "
                 + "WHERE email = '" + req.session.email + "';";
-        con.query(sql,function(err,result){
-            if (err) return res.sendStatus(500)
-            else {
+        executeSql(con, sql)
+            .then((result) => {
                 console.log(result[0].id);
                 var customer_id = result[0].id;
                 console.log(JSON.stringify(newAcct));
@@ -100,33 +96,28 @@ router.post('/addacct', function(req, res, next) {
                 sql = "INSERT INTO account (acct_num, customer_id, routing_num, acct_type, balance_amt, currency, open_date) " 
                     + "VALUES (FLOOR(RAND()*(999999-100000+1)+100000), " + customer_id + ", '" + newAcct.routing_num + "', '" + newAcct.acct_type 
                     +  "', " + newAcct.balance_amt + ", '" + newAcct.currency + "',  current_timestamp())";
-                con.query(sql,function(err,result){
-                    if (err) return res.sendStatus(500)
-                    else {
-                    console.log("Customer " + newAcct.customer_id + " opened a new " + newAcct.acct_type + " account.");
-                    }
-                });
-            }
-        }); 
-
-        // Get new account number for emailing it to customer
-        sql = "SELECT acct_num, balance_amt " 
-            + "From account " 
-            + "ORDER BY open_date DESC " 
-            + "LIMIT 1";
-        con.query(sql,function(err,result){
-            if (err) return res.sendStatus(500)
-            else {
+                console.log("Customer " + newAcct.customer_id + " opened a new " + newAcct.acct_type + " account.");
+                return executeSql(con, sql)
+            })
+            .then(() => {
+                // Get new account number for emailing it to customer
+                sql = "SELECT acct_num, balance_amt "
+                    + "From account "
+                    + "ORDER BY open_date DESC "
+                    + "LIMIT 1";
+                return executeSql(con, sql)
+            })
+            .then((result) => {
                 newAcctNum = result[0].acct_num
                 con.end();
 
                 mailOptions.to = "wei.he@sjsu.edu";
                 // mailOptions.to = req.session.email + "; wei.he@sjsu.edu";
                 mailOptions.subject = "Congratulations!You opened a new " + newAcct.acct_type + " account!";
-                mailOptions.html = "Dear customer, " + "<br /> <br /> " 
-                                    + "Your new <b>" + newAcct.acct_type + "</b> account number is <b>" + newAcctNum + "</b>. <br/><br /> " 
-                                    + "Initial Balance: " + result[0].balance_amt + "<br/><br /> "
-                                    + "Regards, <br /> CMPE-202 Group 3";
+                mailOptions.html = "Dear customer, " + "<br /> <br /> "
+                    + "Your new <b>" + newAcct.acct_type + "</b> account number is <b>" + newAcctNum + "</b>. <br/><br /> "
+                    + "Initial Balance: " + result[0].balance_amt + "<br/><br /> "
+                    + "Regards, <br /> CMPE-202 Group 3";
                 transporter.sendMail(mailOptions, function (error, info) {
                     if (error) {
                         console.log(error);
@@ -136,16 +127,11 @@ router.post('/addacct', function(req, res, next) {
                 });
 
                 res.sendStatus(200)
-            }
-        });
+            })
+            .catch(error => {
+                res.sendStatus(500)
+            })
     });
-    
-
-    
-    // mailOptions.to = newAcct.email + "; wei.he@sjsu.edu";
-
-    // db.close();
-    // }
 })
 
 router.post('/closeacct', function(req, res, next) {
@@ -199,16 +185,19 @@ router.post('/closeacct', function(req, res, next) {
     // db.close();
     // }
 })
-// router.post('/postsignup', function(req, res, next) {
-//     var customer_id = req.body.customer_id
-//     var routing_num =  req.body.routing_num
-//     var acc_type = req.body.accttype
-//     console.log(req.body)
 
-// })
+/**
+ * Execute given `sql` on a sql connection `con`.
+ */
+function executeSql(con, sql) {
+    return new Promise(function(resolve, reject) {
+        con.query(sql, function(err, result){
+            if (err) reject(err)
 
-// router.get('/fundacct', function(req, res, next) {
-//     res.render("fundacct", { title: 'Banking System - Add Account' })
-//   });
+            resolve(result)
+        });
+    })
+    
+}
 
 module.exports = router;
